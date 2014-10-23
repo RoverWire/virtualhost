d060aaedcf05e541d2056083384f77c330e19a52
Cynthia Pereira <cynthiacpereira@gmail.com>
2014-10-23 18:06:16 -0200

    Internationalization & Localization

---
+++ locale/en/virtualhost.pot
@@ -0,0 +1,59 @@
+msgid ""
+msgstr ""
+"Project-Id-Version: Virtualhost Manage Script\n"
+"Last-Translator: \n"
+"MIME-Version: 1.0\n"
+"Content-Type: text/plain; charset=UTF-8\n"
+"Content-Transfer-Encoding: 8bit\n"
+
+#: virtualhost.sh:19
+msgid "You have no permission to run $0 as non-root user. Use sudo"
+msgstr ""
+#: virtualhost.sh:25
+msgid "You need to prompt for action (create or delete) -- Lower-case only"
+msgstr ""
+#: virtualhost.sh:31
+msgid "Please provide domain. e.g.dev,staging"
+msgstr ""
+#: virtualhost.sh:43
+msgid "This domain already exists.\\nPlease Try Another one"
+msgstr ""
+#: virtualhost.sh:56
+msgid "ERROR: Not able to write in file $userDir/$rootdir/phpinfo.php. Please check permissions."
+msgstr ""
+#: virtualhost.sh:59
+msgid "Added content to $userDir$rootdir/phpinfo.php."
+msgstr ""
+#: virtualhost.sh:83
+msgid "There is an ERROR create $domain file"
+msgstr ""
+#: virtualhost.sh:86
+msgid "\\nNew Virtual Host Created\\n"
+msgstr ""
+#: virtualhost.sh:92
+msgid "ERROR: Not able write in /etc/hosts"
+msgstr ""
+#: virtualhost.sh:95
+msgid "Host added to /etc/hosts file \\n"
+msgstr ""
+#: virtualhost.sh:111
+msgid "Complete! \\nYou now have a new Virtual Host \\nYour new host is: http://$domain \\nAnd its located at $userDir$rootdir"
+msgstr ""
+#: virtualhost.sh:116
+msgid "This domain dont exists.\\nPlease Try Another one"
+msgstr ""
+#: virtualhost.sh:135
+msgid "Delete host root directory ? (s/n)"
+msgstr ""
+#: virtualhost.sh:141
+msgid "Directory deleted"
+msgstr ""
+#: virtualhost.sh:143
+msgid "Host directory conserved"
+msgstr ""
+#: virtualhost.sh:146
+msgid "Host directory not found. Ignored"
+msgstr ""
+#: virtualhost.sh:150
+msgid "Complete!\\nYou just removed Virtual Host $domain"
+msgstr ""
--- virtualhost.sh
+++ virtualhost.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+### Set Language
+TEXTDOMAIN=virtualhost
+
 ### Set default parameters
 action=$1
 domain=$2
@@ -7,40 +10,40 @@ owner=$(who am i | awk '{print $1}')
 email='webmaster@localhost'
 sitesEnable='/etc/apache2/sites-enabled/'
 sitesAvailable='/etc/apache2/sites-available/'
-userDir='/var/www/'
+userDir='/srv/'
 sitesAvailabledomain=$sitesAvailable$domain.conf
 
 ### don't modify from here unless you know what you are doing ####
- 
+
 if [ "$(whoami)" != 'root' ]; then
-  	echo "You have no permission to run $0 as non-root user. Use sudo"
+  	echo $"You have no permission to run $0 as non-root user. Use sudo"
 		exit 1;
 fi
- 
-if [ "$action" != 'create' ] && [ "$action" != 'delete' ] 
+
+if [ "$action" != 'create' ] && [ "$action" != 'delete' ]
 	then
-		echo "You need to prompt for action (create or delete) -- Lower-case only"
+		echo $"You need to prompt for action (create or delete) -- Lower-case only"
 		exit 1;
 fi
- 
+
 while [ "$domain" == ""  ]
 do
-	echo -e "Please provide domain. e.g.dev,staging"
+	echo -e $"Please provide domain. e.g.dev,staging"
 	read  domain
 done
- 
+
 if [ "$rootdir" == "" ]; then
 	rootdir=${domain//./}
 fi
- 
-if [ "$action" == 'create' ] 
+
+if [ "$action" == 'create' ]
 	then
 		### check if domain already exists
 		if [ -e $sitesAvailabledomain ]; then
-			echo -e 'This domain already exists.\nPlease Try Another one'
+			echo -e $"This domain already exists.\nPlease Try Another one"
 			exit;
 		fi
- 
+
 		### check if directory exists or not
 		if ! [ -d $userDir$rootdir ]; then
 			### create the directory
@@ -50,13 +53,13 @@ if [ "$action" == 'create' ]
 			### write test file in the new domain dir
 			if ! echo "<?php echo phpinfo(); ?>" > $userDir$rootdir/phpinfo.php
 			then
-				echo "ERROR: Not able to write in file "$userDir"/"$rootdir"/phpinfo.php. Please check permissions."
+				echo $"ERROR: Not able to write in file $userDir/$rootdir/phpinfo.php. Please check permissions."
 				exit;
 			else
-				echo "Added content to "$userDir$rootdir"/phpinfo.php."
+				echo $"Added content to $userDir$rootdir/phpinfo.php."
 			fi
 		fi
- 
+
 		### create virtual host rules file
 		if ! echo "
 		<VirtualHost *:80>
@@ -77,21 +80,21 @@ if [ "$action" == 'create' ]
 			CustomLog /var/log/apache2/$domain-access.log combined
 		</VirtualHost>" > $sitesAvailabledomain
 		then
-			echo -e 'There is an ERROR create $domain file'
+			echo -e $"There is an ERROR create $domain file"
 			exit;
 		else
-			echo -e '\nNew Virtual Host Created\n'
+			echo -e $"\nNew Virtual Host Created\n"
 		fi
- 
+
 		### Add domain in /etc/hosts
 		if ! echo "127.0.0.1	$domain" >> /etc/hosts
 		then
-			echo "ERROR: Not able write in /etc/hosts"
+			echo $"ERROR: Not able write in /etc/hosts"
 			exit;
 		else
-			echo -e "Host added to /etc/hosts file \n"
+			echo -e $"Host added to /etc/hosts file \n"
 		fi
- 
+
 		if [ "$owner" == ""  ]; then
 			chown -R $(whoami):$(whoami) $userDir$rootdir
 		else
@@ -100,17 +103,17 @@ if [ "$action" == 'create' ]
 
 		### enable website
 		a2ensite $domain
- 
+
 		### restart Apache
 		/etc/init.d/apache2 reload
- 
+
 		### show the finished message
-		echo -e "Complete! \nYou now have a new Virtual Host \nYour new host is: http://"$domain" \nAnd its located at "$userDir$rootdir
+		echo -e $"Complete! \nYou now have a new Virtual Host \nYour new host is: http://$domain \nAnd its located at $userDir$rootdir"
 		exit;
 	else
 		### check whether domain already exists
 		if ! [ -e $sitesAvailabledomain ]; then
-			echo -e 'This domain dont exists.\nPlease Try Another one'
+			echo -e $"This domain dont exists.\nPlease Try Another one"
 			exit;
 		else
 			### Delete domain in /etc/hosts
@@ -119,31 +122,31 @@ if [ "$action" == 'create' ]
 
 			### disable website
 			a2dissite $domain
-	 
+
 			### restart Apache
 			/etc/init.d/apache2 reload
 
 			### Delete virtual host rules files
 			rm $sitesAvailabledomain
 		fi
- 
+
 		### check if directory exists or not
 		if [ -d $userDir$rootdir ]; then
-			echo -e 'Delete host root directory ? (s/n)'
+			echo -e $"Delete host root directory ? (s/n)"
 			read deldir
 
 			if [ "$deldir" == 's' -o "$deldir" == 'S' ]; then
 				### Delete the directory
 				rm -rf $userDir$rootdir
-				echo -e 'Directory deleted'
+				echo -e $"Directory deleted"
 			else
-				echo -e 'Host directory conserved'
+				echo -e $"Host directory conserved"
 			fi
 		else
-			echo -e 'Host directory not found. Ignored'
+			echo -e $"Host directory not found. Ignored"
 		fi
- 
+
 		### show the finished message
-		echo -e "Complete!\nYou just removed Virtual Host "$domain
+		echo -e $"Complete!\nYou just removed Virtual Host $domain"
 		exit 0;
 fi
