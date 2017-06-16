46239f8c3ec1c02e962baf954b256a634d530164
Jacob Bearce <jacob@bearce.me>
2017-06-16 15:58:58 -0500

    Change spaces to tabs for consistency

--- virtualhost.sh
+++ virtualhost.sh
@@ -102,16 +102,16 @@ if [ "$action" == 'create' ]
 			echo -e $"Host added to /etc/hosts file \n"
 		fi
 
-        ### Add domain in /mnt/c/Windows/System32/drivers/etc/hosts (Windows Subsytem for Linux)
-        if [ -e /mnt/c/Windows/System32/drivers/etc/hosts ]
-        then
-            if ! echo "127.0.0.1	$domain" >> /mnt/c/Windows/System32/drivers/etc/hosts
-            then
-                echo $"ERROR: Not able to write in /mnt/c/Windows/System32/drivers/etc/hosts"
-            else
-                echo -e $"Host added to /mnt/c/Windows/System32/drivers/etc/hosts file \n"
-            fi
-        fi
+		### Add domain in /mnt/c/Windows/System32/drivers/etc/hosts (Windows Subsytem for Linux)
+		if [ -e /mnt/c/Windows/System32/drivers/etc/hosts ]
+		then
+			if ! echo "127.0.0.1	$domain" >> /mnt/c/Windows/System32/drivers/etc/hosts
+			then
+				echo $"ERROR: Not able to write in /mnt/c/Windows/System32/drivers/etc/hosts"
+			else
+				echo -e $"Host added to /mnt/c/Windows/System32/drivers/etc/hosts file \n"
+			fi
+		fi
 
 		if [ "$owner" == "" ]; then
 			chown -R $(whoami):$(whoami) $rootDir
@@ -138,12 +138,12 @@ if [ "$action" == 'create' ]
 			newhost=${domain//./\\.}
 			sed -i "/$newhost/d" /etc/hosts
 
-            ### Delete domain in /mnt/c/Windows/System32/drivers/etc/hosts (Windows Subsytem for Linux)
-            if [ -e /mnt/c/Windows/System32/drivers/etc/hosts ]
-            then
-                newhost=${domain//./\\.}
-    			sed -i "/$newhost/d" /mnt/c/Windows/System32/drivers/etc/hosts
-            fi
+			### Delete domain in /mnt/c/Windows/System32/drivers/etc/hosts (Windows Subsytem for Linux)
+			if [ -e /mnt/c/Windows/System32/drivers/etc/hosts ]
+			then
+				newhost=${domain//./\\.}
+				sed -i "/$newhost/d" /mnt/c/Windows/System32/drivers/etc/hosts
+			fi
 
 			### disable website
 			a2dissite $domain
